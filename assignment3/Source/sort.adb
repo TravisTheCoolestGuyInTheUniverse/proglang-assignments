60c74042ec3a2882268ac0517567a01f44dde11f
TravisTheCoolestGuyInTheUniverse <sutopu@DESKTOP-I353F3M.localdomain>
2020-11-12 23:23:03 -0500

    successfully implemented exchange sorting algorithm in ada programming language. next step is to make it generic.

--- assignment3/Source/sort.adb
+++ assignment3/Source/sort.adb
@@ -1,21 +1,36 @@
 with Text_Io;
 use Text_Io;
 
-procedure Main is
-    Temp : Integer := 0;
+procedure Sort is
     Length : Integer := Integer'value(Get_Line);
-    type RAY is arRAY(1..Length) of INTEGER;
+    type RAY is ARRAY(1..Length) of INTEGER;
     Data : RAY;
 
-    
+    function SortArray(Data : in out RAY) return RAY is
+        Temp : Integer := 0;
+    begin
+        for I in 1..Length loop
+            for J in I..Length loop
+                if Data(I) > Data(J) then
+                    Temp := Data(I);
+                    Data(I) := Data(J);
+                    Data(J) := Temp;
+                end if;
+            end loop;
+        end loop;
+        return Data;
+    end SortArray;
+
 begin
     --get the length of the array
     for I in 1..Length loop
         Data(I) := Integer'value(Get_Line);
     end loop;
 
+    Data := SortArray(Data);
+
     for I in 1..Length loop 
         put(Integer'Image(Data(I)));
     end loop;
 
-end Main;
+end Sort;
